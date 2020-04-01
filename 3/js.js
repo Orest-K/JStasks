694df3d0d40db9f470b7a4e5f58cdf18abd16c01
Orest-K <orest2558@gmail.com>
2020-04-01 21:35:16 +0300

    test

---
+++ 3/js.js
@@ -0,0 +1,60 @@
+// рішення першого завдання
+// let cont1 = document.getElementById('content');
+// console.log(cont1);
+// cont1.innerHTML = `some costum text element`;
+// let cont2 = document.getElementById('rules');
+// console.log(cont2);
+// cont2.innerHTML = `some costum text element22222`;
+// let bodyGolovna = document.getElementsByTagName(`body`)[0];
+// console.log(bodyGolovna);
+// bodyGolovna.style.color =`blue`;
+// bodyGolovna.style.backgroundColor =`#FA8072`;
+// console.log(cont2.classList);
+// let rules = document.getElementsByClassName('fc_rules');
+// console.log(rules);
+// for (let item of rules){
+//     item.style.color='red';}
+
+// рішення 2 завдання
+// let headElement = document.getElementById('main_header');
+// headElement.style.color='green';
+// let ulList = document.getElementsByTagName('ul')[0];
+// console.log(ulList);
+// ulList.style.width ='400px';
+// let linkList1 = document.getElementsByClassName('linkList');
+// console.log(linkList1);
+// for(let elem of linkList1){
+//     elem.style.width = '50%';
+// }
+// let someText = document.querySelector('.listElement2');
+// console.log(someText.innerText);
+// let someLists = document.getElementsByTagName('li');
+// for (let list of someLists){
+//     list.style.backgroundColor='gray';
+// }
+// let ancors = document.getElementsByTagName('a');
+// for(let ank of ancors){
+//     ank.classList.add("anchor");
+// if(ank.innerText ==='link3'){
+//        ank.style.fontSize = '40px';
+//     }
+//     let xxx = ank.innerText;
+//    ank.classList.add(`element_${xxx}`);
+// }
+// let subHed = document.getElementsByClassName('sub-header');
+// let customCol = prompt("enter color like 'red' or '#8B0000'");
+// for(let v of subHed){
+// v.style.backgroundColor = customCol;
+// if(v.innerText === 'content 2 segment' ){
+//     v.style.color = prompt("enter color for text like 'red' or '#8B0000'");
+// }
+// }
+// let ct = document.getElementsByClassName('content_1')[0];
+// ct.innerHTML = prompt("enter some text");
+// let customP = document.getElementsByTagName('p');
+// for(let i of customP){
+//     let z = Math.random()*20;
+//     i.style.padding = (`${z}px`);
+// }
+// let customClass = document.querySelector('.text2');
+// customClass.textContent = 'idsotjghb';
